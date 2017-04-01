6729ddcfa4bed9ceec446dc10d4fee7f16389434
sreeharim <sreeharim1992@gmail.com>
2017-04-01 12:18:32 +0530

    Adding files
    Adding files

---
+++ platforms/browser/www/js/services.js
@@ -0,0 +1,29 @@
+app.factory("DateService",function($log){
+	return {
+		getFormattedTime:  function(){
+			var  months= ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+			var d = new Date();
+			var hours = d.getHours();
+			var day = d.getDate();
+			var month =months[d.getMonth()];
+			var minutes = d.getMinutes();
+			var sec = d.getSeconds();
+			var year= d.getFullYear();
+			if(minutes<10)
+				minutes = '0'+minutes;
+			if(sec<10)
+				sec = '0'+sec;
+    		hours = (hours+24)%24; 
+    		var mid='AM';
+    		if(hours==0){ 
+    			hours=12;
+    		}
+    		else if(hours>12)
+    		{
+    			hours=hours%12;
+    			mid='PM';
+    		}
+			return month+' '+day+','+year+' '+hours+':'+minutes+':'+sec+' '+mid;
+		}
+	}
+});
---
+++ www/js/controller.js
@@ -0,0 +1,133 @@
+app.controller("myController",function($scope,$log,DateService){
+				   	$scope.order = ['primary','success','info','warning','danger'];
+                    $scope.cards = [];
+                    $scope.times=[];
+                    $scope.count = 0;
+                    $scope.showEditArea = false;
+                    $scope.updateMode = false;
+                    $scope.currUpdateId = '';
+                    $scope.currentText ='';
+                    $scope.store = Lawnchair({name: 'db' }, function(e){
+                            console.log('Storage open');
+                            });
+                      $scope.showEdit = function(){
+                        $scope.showEditArea = true;
+                      
+                      }
+
+                    $scope.save = function(){
+                        if($scope.updateMode)
+                            $scope.updateCard();
+                        else{
+                            var time = DateService.getFormattedTime();
+                            $scope.times[$scope.count]=time;
+                            $scope.classSelector = $scope.count%5;
+                            $scope.cards.unshift(new Card($scope.count,$scope.currentText));
+                            $scope.showEditArea = false; 
+                            var obj = {
+                                'id':$scope.count,
+                                'header':time,
+                                'content':$scope.currentText
+                            }
+                            $scope.store.save({ value: obj });
+                            $scope.currentText='';
+                            $scope.count++;
+                        }
+                        
+                        }
+                    $scope.cancel = function(){
+                        $scope.showEditArea = false;  
+                        $scope.updateMode = false;
+                        $scope.currUpdateId = '';
+                        $scope.currentText ='';
+                        }
+                    $scope.init = function(){
+                        $scope.store.each(function(record, index) {
+                             console.log(record.value.header);
+                              $scope.classSelector = $scope.count%5;
+                              $scope.times[$scope.count]=record.value.header;
+                              $scope.cards.unshift(new Card($scope.count,record.value.content));
+                              $scope.count++;
+                        })
+                        
+                    }
+                    $scope.reset =function(){
+                        $scope.cards = [];
+                        $scope.times=[];
+                        $scope.count = 0;
+                        $scope.currentText ='';
+                    }
+                    $scope.editCard = function(id){
+                        $scope.updateMode = true;
+                        $scope.currUpdateId = id;
+                        $scope.showEditArea = true;
+                        for(var i in $scope.cards)
+                            if($scope.cards[i].id==id){
+                                $scope.currentText = $scope.cards[i].text;
+                                break;
+                            }
+                    }
+                    $scope.updateCard = function(){
+                        
+                        for(var i in $scope.cards)
+                            if($scope.cards[i].id==$scope.currUpdateId){
+                                $scope.cards[i].text = $scope.currentText;
+                                break;
+                            }
+                        $scope.store.each(function(record, index) {
+                                  if($scope.currUpdateId == record.value.id){
+                                     record.value.content=$scope.currentText;
+                                     $scope.store.save({key:record.key,value:record.value});
+                                  }
+                                                           
+                        })
+                     
+                        $scope.updateMode = false;
+                        $scope.currUpdateId = '';
+                        $scope.showEditArea = false;
+                        $scope.currentText ='';
+                    }                
+                    $scope.deleteCard = function(id){
+                        var index = -1;
+                        for(var i in $scope.cards)
+                            if($scope.cards[i].id==id){
+                                index=i;
+                                break;
+                            }
+                            if(index!=-1){
+                                $scope.cards.splice(index,1);
+
+                            }
+                              
+                        $scope.store.each(function(record, index) {
+                                  if(id == record.value.id)
+                                     $scope.store.remove(record.key);
+                                                           
+                        })
+                    }
+
+                    
+                    $scope.deleteAll = function(){
+                        $scope.store.keys( function(key) {
+                            console.log('Removed '+key);
+                            $scope.store.remove(key);})
+                    }
+                    //$scope.deleteAll();
+                    $scope.init();
+                    })
+    .directive("myCard",function(){
+        return{
+            restrict: "A",
+            scope:true,
+            link: function(scope){
+                console.log("Called directive");
+                scope.myTemplate  = 'templates/card-'+scope.order[scope.card.id%5]+ '.html';
+                },
+           template:'<div ng-include="myTemplate"></div>'
+         }
+    });
+
+      function Card(id,text) {
+            this.id = id;
+            this.text = text;
+        }
